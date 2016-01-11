a0afc1cd208dfc352c23edf53500515c27c183e2
Salahuddin <sagor@skku.edu>
2016-01-11 14:27:49 +0900

    Final version of problem 113

---
+++ Uva_113/src/Main/Main.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Salahuddin
+ */
+class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out, true);
+
+        // while there is some input to read
+        while (in.hasNextDouble()) {
+            double n = in.nextDouble();
+            double p = in.nextDouble();
+
+            out.printf("%.0f\n", Math.pow(p, 1 / n));
+        }
+    }
+}
